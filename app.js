3573de5ca339e3699149097f6dd0531099a675fc
AmineKARA <aminekaramos@gmail.com>
2020-01-20 13:18:58 +0100

    Updated back

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ const routeUsers = require('./src/compUsers/routesUsers');
 const routeItineraires = require('./src/comipItineraires/routesItineraires');
 const routeAvis = require('./src/compAvis/routesAvis');
 
-var cors = require('cors')
-app.use(cors())
+//Définition des CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+        res.setHeader ("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Max-Age", "1800");
+        res.setHeader("Access-Control-Allow-Headers", "content-type");
+        res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+      
+        //res.Header("Access-Control-Allow-Origin","http://localhost:3030/user/login");
+        
+        next();
+});
 
 // body parser 
 app.use(bodyParser.urlencoded({ extended: true }));
